bf92067ca2fa2cd2e22866254b866e4bda7003a7
Nick Jacobson <33440907+nick-jac@users.noreply.github.com>
2017-12-28 16:41:14 +1300

    added some very dodgy coding for some not very eloquent js

--- eloquent.js
+++ eloquent.js
@@ -1,1 +1,34 @@
+//minimum
 
+console.log(Math.min(0, 10))
+console.log(Math.min(0, -10))
+
+//recursion
+
+function isEven(num) {
+    if (num % 0)
+        return true;
+      else return false;
+   }
+   
+  
+  
+  
+  console.log(isEven(50));
+  // → true
+  console.log(isEven(75));
+  // → false
+  console.log(isEven(-1));
+  // → ??
+
+
+//bean counting
+
+  function countBs(B){
+    string.charAt(B);
+    }
+    
+    console.log(countBs("BBC"));
+    // → 2
+    console.log(countChar("kakkerlak", "k"));
+    // → 4
